c1ed9d9d295d63844c4f45666fafde1af33e06e1
SopheakWEP <sopheak.riths@gmail.com>
2018-06-06 10:47:02 +0700

    [RCI001] Create footer for site refs RCI001

--- wp-content/themes/RCI/header.php
+++ wp-content/themes/RCI/header.php
@@ -5,6 +5,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- wp-content/themes/RCI/index.php
+++ wp-content/themes/RCI/index.php
@@ -20,3 +20,4 @@
 		</div>
 	</div>
 </div>
+<?php get_footer(); ?>
