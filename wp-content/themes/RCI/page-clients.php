91d9793c925fcf0207e1de38a2fa4ca3c77638f0
devit-chea <davit.chea123@gmail.com>
2018-06-19 16:15:01 +0700

    delete and change

--- wp-content/themes/RCI/page-clients.php
+++ wp-content/themes/RCI/page-clients.php
@@ -1,168 +1,167 @@
-
-			<?php get_header(); ?>
-			<div class="warp-contain">
-				<div class="rci-client heading-content">
-					<img class="img-responsive" id="background-image-client" src="http://rciapac.com/wp-content/uploads/2017/03/645.jpg" alt="">
-					<div class="outer-heading">
-						<div class="middle-heading">
-							<div class="inner-heading text-decription-1">
-								<div class="container">
-									<div class="row">
-										<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12"></div>
-										<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-											<h1 class="text-center wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s" style="color: #000;">RCI's Clients</h1>
-											<h3 class="text-center text-2 wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s" id="fade-border" style="color: #000; line-height: 3;" >We are proud to be trusted by our Clients</h3>
-											<div class="wow slideInUp" data-wow-duration="1.5s"data-wow-delay="0.4s">
-												<!-- <hr style="width: 150px;"> -->
-											</div>
-											<p class="text-center-1 wow slideInUp" data-wow-duration="2s" data-wow-delay="0.5s" style="color: #000;">
-												At RCI, we provide solutions and ensure that it fits our client’s expectations. We provide tailored services and we supervise the entire process, from the consulting to the support. Our core activities consist of advising, helping retailers to find solutions and implementing them through project management.
-											</p>
-										</div>
-										<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12"></div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="respone-content">
-						<div class="container text-decription-responsive">
+	<?php get_header(); ?>
+	<div class="warp-contain">
+		<div class="rci-client heading-content">
+			<img class="img-responsive" id="background-image-client" src="http://rciapac.com/wp-content/uploads/2017/03/645.jpg" alt="">
+			<div class="outer-heading">
+				<div class="middle-heading">
+					<div class="inner-heading text-decription-1">
+						<div class="container">
 							<div class="row">
-								<div class="col-sm-12 col-xs-12"></div>
-								<div class="col-sm-12 col-xs-12">
-									<h1 class="text-center wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s" style="color: #fff;">RCI's Clients</h1>
-									<h3 class="text-center text-2 wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s" id="fade-border" style="color: #fff;font-size:4vw; line-height: 3;" >We are proud to be trusted by our Clients</h3>
+								<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12"></div>
+								<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+									<h1 class="text-center wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s" style="color: #000;">RCI's Clients</h1>
+									<h3 class="text-center text-2 wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s" id="fade-border" style="color: #000; line-height: 3;" >We are proud to be trusted by our Clients</h3>
 									<div class="wow slideInUp" data-wow-duration="1.5s"data-wow-delay="0.4s">
 										<!-- <hr style="width: 150px;"> -->
 									</div>
-									<p class="text-center-1 wow slideInUp" data-wow-duration="2s" data-wow-delay="0.5s" style="color: #fff;">
+									<p class="text-center-1 wow slideInUp" data-wow-duration="2s" data-wow-delay="0.5s" style="color: #000;">
 										At RCI, we provide solutions and ensure that it fits our client’s expectations. We provide tailored services and we supervise the entire process, from the consulting to the support. Our core activities consist of advising, helping retailers to find solutions and implementing them through project management.
 									</p>
 								</div>
-								<div class="col-sm-12 col-xs-12"></div>
+								<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12"></div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div class="container">
-					<div class="row per-detail">
-						<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12"></div>
-						<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-							<h2 class="text-center">Perfection in every detail</h2>
-							<p class="text-center-1">
-								As the market for luxury goods and services continues to grow worldwide, world’s acclaimed luxury brands can rely on the actionable insights and customized online retail platforms offered by RCI Global Services to reach their target consumers more effectively and efficiently.
-							</p>
-							<p class="text-center-1">
-								In 2010 when RCI Global Services was newly established, the leading chain of perfume and cosmetics stores in France, Sephora, laid their faith in RCI Global Services with its specialized retail & IT solutions. Since then, Aigle, Estée Lauder, Gucci and Fendi have become RCI Global Services’ clients. Confidence and trust among the most prestigious international retailers have been built sustainably, and Pandora is its most recent ally with their impressive expansion in the APAC area.
+			</div>
+			<div class="respone-content">
+				<div class="container text-decription-responsive">
+					<div class="row">
+						<div class="col-sm-12 col-xs-12"></div>
+						<div class="col-sm-12 col-xs-12">
+							<h1 class="text-center wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s" style="color: #fff;">RCI's Clients</h1>
+							<h3 class="text-center text-2 wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s" id="fade-border" style="color: #fff;font-size:4vw; line-height: 3;" >We are proud to be trusted by our Clients</h3>
+							<div class="wow slideInUp" data-wow-duration="1.5s"data-wow-delay="0.4s">
+								<!-- <hr style="width: 150px;"> -->
+							</div>
+							<p class="text-center-1 wow slideInUp" data-wow-duration="2s" data-wow-delay="0.5s" style="color: #fff;">
+								At RCI, we provide solutions and ensure that it fits our client’s expectations. We provide tailored services and we supervise the entire process, from the consulting to the support. Our core activities consist of advising, helping retailers to find solutions and implementing them through project management.
 							</p>
 						</div>
-						<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12"></div>
+						<div class="col-sm-12 col-xs-12"></div>
 					</div>
 				</div>
-				<div class="container client-menu">
-					<div class="row grid-img">
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/03/Gucci-2-420x320.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">gucci</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/FENDI-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">fendi</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/CELINE-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">CÉLINE</h2>
-							</div>
-						</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row per-detail">
+				<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12"></div>
+				<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+					<h2 class="text-center">Perfection in every detail</h2>
+					<p class="text-center-1">
+						As the market for luxury goods and services continues to grow worldwide, world’s acclaimed luxury brands can rely on the actionable insights and customized online retail platforms offered by RCI Global Services to reach their target consumers more effectively and efficiently.
+					</p>
+					<p class="text-center-1">
+						In 2010 when RCI Global Services was newly established, the leading chain of perfume and cosmetics stores in France, Sephora, laid their faith in RCI Global Services with its specialized retail & IT solutions. Since then, Aigle, Estée Lauder, Gucci and Fendi have become RCI Global Services’ clients. Confidence and trust among the most prestigious international retailers have been built sustainably, and Pandora is its most recent ally with their impressive expansion in the APAC area.
+					</p>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12"></div>
+			</div>
+		</div>
+		<div class="container client-menu">
+			<div class="row grid-img">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/03/Gucci-2-420x320.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">gucci</h2>
 					</div>
-					<div class="row grid-img">
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/ESTEE-LAUDER-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">ESTEE LAUDER</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/ESCADA-CLIENTS-2.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">ESCADA</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/03/Lancome6402-420x320.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">Lancôme</h2>
-							</div>
-						</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/FENDI-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">fendi</h2>
 					</div>
-					<div class="row grid-img">
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/Kiehls-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">KIEHL’S</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/15036700_1048055528640470_4647953771471209309_n-420x320.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">Urban Decay</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/cruise17-filfury-campaign-finalpreview-1-20161214200710-420x320.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">PHILIPP PLEIN</h2>
-							</div>
-						</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/CELINE-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">CÉLINE</h2>
 					</div>
-					<div class="row grid-img">
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/GODIVA-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">GODIVA</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/03/Pandora-1.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">Pandora</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/Mikmoto-420x320.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">MIKIMOTO</h2>
-							</div>
-						</div>
+				</div>
+			</div>
+			<div class="row grid-img">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/ESTEE-LAUDER-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">ESTEE LAUDER</h2>
 					</div>
-					<div class="row grid-img">
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/03/17499256_1346963788680448_7426354829070525987_n-420x320.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">MONNALISA</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/05/18194817_1395455037188899_6260793893102066205_n-1-420x320.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">CRABTREE & EVELYN</h2>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
-							<img src="http://rciapac.com/wp-content/uploads/2017/04/Innisfree-1-420x320.jpg" alt="" style="width: 100%; height: auto">
-							<div class="button-title">
-								<h2 class="client-title">Innisfree</h2>
-							</div>
-						</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/ESCADA-CLIENTS-2.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">ESCADA</h2>
 					</div>
-
-				</div>	
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/03/Lancome6402-420x320.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">Lancôme</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row grid-img">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/Kiehls-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">KIEHL’S</h2>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/15036700_1048055528640470_4647953771471209309_n-420x320.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">Urban Decay</h2>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/cruise17-filfury-campaign-finalpreview-1-20161214200710-420x320.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">PHILIPP PLEIN</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row grid-img">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/GODIVA-CLIENTS-1.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">GODIVA</h2>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/03/Pandora-1.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">Pandora</h2>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/Mikmoto-420x320.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">MIKIMOTO</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row grid-img">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/03/17499256_1346963788680448_7426354829070525987_n-420x320.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">MONNALISA</h2>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/05/18194817_1395455037188899_6260793893102066205_n-1-420x320.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">CRABTREE & EVELYN</h2>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 spaec-img wow slideInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
+					<img src="http://rciapac.com/wp-content/uploads/2017/04/Innisfree-1-420x320.jpg" alt="" style="width: 100%; height: auto">
+					<div class="button-title">
+						<h2 class="client-title">Innisfree</h2>
+					</div>
+				</div>
 			</div>
 
-			<?php get_footer(); ?>
-			
+		</div>	
+	</div>
+
+	<?php get_footer(); ?>
+	
---
+++ wp-content/themes/RCI/page-parters.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+	<div class="warp-contain">
+		<div class="side-contant-area">
+			.header-banner
+		</div>
+	</div>
+<?php get_footer(); ?>
