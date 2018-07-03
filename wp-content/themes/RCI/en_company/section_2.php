a2e9eed3b335252279170bc241bb460003bf90a1
devit-chea <davit.chea123@gmail.com>
2018-07-03 11:25:05 +0700

    [D0025] change some style error in page refs D0025

--- wp-content/themes/RCI/en_company/section_2.php
+++ wp-content/themes/RCI/en_company/section_2.php
@@ -169,3 +169,86 @@ include 'config.php';
 
 <!-- end of heading banner -->
 		<!-- start slide click -->
+
+<!-- <section>
+	<div class="chronology owl-carousel owl-chronology-nav owl-theme owl-loaded owl-drag">
+		<div class="owl-stage-outer owl-height" style="height: 636px;">
+			<div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: 0s; width: 8154px;">
+				<div class="owl-item active" style="width: 1349px; margin-right: 10px;">
+					<div class="chronology-item text-center item">
+						<img width="1366" height="636" src="http://rciapac.com/wp-content/uploads/2017/03/HK-3.jpg" class="attachment-1366x676 size-1366x676 wp-post-image" alt="" srcset="http://rciapac.com/wp-content/uploads/2017/03/HK-3.jpg 1440w, http://rciapac.com/wp-content/uploads/2017/03/HK-3-300x140.jpg 300w, http://rciapac.com/wp-content/uploads/2017/03/HK-3-768x357.jpg 768w, http://rciapac.com/wp-content/uploads/2017/03/HK-3-1024x476.jpg 1024w, http://rciapac.com/wp-content/uploads/2017/03/HK-3-193x90.jpg 193w, http://rciapac.com/wp-content/uploads/2017/03/HK-3-107x50.jpg 107w" sizes="(max-width: 1366px) 100vw, 1366px">			
+						<div class="description">
+							<h2>Chronology</h2>
+							<h3>History of RCI Global Services – 2010</h3>
+							<div class="span span_text_detailt">
+								<p style="text-align: justify;">RCI Global Services was created in 2010 after realizing the lack of IT expertise in Asia-Pacific’s luxury retail industry. Its mission is to offer European and US technology and proficiency, combined with local knowledge and awareness. In merely 7 years, RCI Global Services has gained the unwavering trust of the most prestigious international retailers including Gucci, Fendi, Valentino, Céline, Bottega Veneta or&nbsp;Godiva.</p>
+								<p style="text-align: justify;">As a leading retail consulting firm and IT solution provider in APAC, RCI Global Services is specialized in superior IT architecture design, end-to-end project implementation, and manages services for international luxury retail industry, the pinnacle of all retail business.</p>
+								<p style="text-align: justify;">RCI Global Services is one of the first European companies to offer customized retail solutions with added value for the international luxury industry in APAC. As a premier services company which&nbsp;always exceed customers’ expectations, its customers are its top priority and your success is RCI Global Services’ objective. When you succeed, RCI Global Services succeeds.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="owl-item" style="width: 1349px; margin-right: 10px;">
+					<div class="chronology-item text-center item">
+						<img width="1366" height="636" src="http://rciapac.com/wp-content/uploads/2017/03/Gucci-3.jpg" class="attachment-1366x676 size-1366x676 wp-post-image" alt="" srcset="http://rciapac.com/wp-content/uploads/2017/03/Gucci-3.jpg 1440w, http://rciapac.com/wp-content/uploads/2017/03/Gucci-3-300x140.jpg 300w, http://rciapac.com/wp-content/uploads/2017/03/Gucci-3-768x357.jpg 768w, http://rciapac.com/wp-content/uploads/2017/03/Gucci-3-1024x476.jpg 1024w, http://rciapac.com/wp-content/uploads/2017/03/Gucci-3-193x90.jpg 193w, http://rciapac.com/wp-content/uploads/2017/03/Gucci-3-107x50.jpg 107w" sizes="(max-width: 1366px) 100vw, 1366px">
+						<div class="description">
+							<h2>Chronology</h2>
+							<h3>Project Gucci – 2012</h3>
+							<div class="span span_text_detailt">
+								<p style="text-align: justify;"><span style="font-size: 12pt;">In 2012, the Gucci project (Kering Group) was launched. It consisted in implementing POS solutions for Gucci in several countries in Asia-Pacific. The solution chosen was Oracle Retail Xstore Point of sale.&nbsp;</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="owl-item" style="width: 1349px; margin-right: 10px;">
+					<div class="chronology-item text-center item">
+						<img width="1366" height="636" src="http://rciapac.com/wp-content/uploads/2017/04/Shenzhen.jpg" class="attachment-1366x676 size-1366x676 wp-post-image" alt="" srcset="http://rciapac.com/wp-content/uploads/2017/04/Shenzhen.jpg 1440w, http://rciapac.com/wp-content/uploads/2017/04/Shenzhen-300x140.jpg 300w, http://rciapac.com/wp-content/uploads/2017/04/Shenzhen-768x357.jpg 768w, http://rciapac.com/wp-content/uploads/2017/04/Shenzhen-1024x476.jpg 1024w, http://rciapac.com/wp-content/uploads/2017/04/Shenzhen-193x90.jpg 193w, http://rciapac.com/wp-content/uploads/2017/04/Shenzhen-107x50.jpg 107w" sizes="(max-width: 1366px) 100vw, 1366px">
+						<div class="description">
+							<h2>Chronology</h2>
+							<h3>New office in Shenzhen, China – 2013</h3>
+							<div class="span span_text_detailt"><p style="text-align: justify;"><span style="font-size: 12pt;">RCI opens its first office abroad in Shenzhen, China.</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="owl-item" style="width: 1349px; margin-right: 10px;">
+					<div class="chronology-item text-center item">
+						<img width="1366" height="636" src="http://rciapac.com/wp-content/uploads/2017/04/Seoul-1.jpg" class="attachment-1366x676 size-1366x676 wp-post-image" alt="" srcset="http://rciapac.com/wp-content/uploads/2017/04/Seoul-1.jpg 1440w, http://rciapac.com/wp-content/uploads/2017/04/Seoul-1-300x140.jpg 300w, http://rciapac.com/wp-content/uploads/2017/04/Seoul-1-768x357.jpg 768w, http://rciapac.com/wp-content/uploads/2017/04/Seoul-1-1024x476.jpg 1024w, http://rciapac.com/wp-content/uploads/2017/04/Seoul-1-193x90.jpg 193w, http://rciapac.com/wp-content/uploads/2017/04/Seoul-1-107x50.jpg 107w" sizes="(max-width: 1366px) 100vw, 1366px">
+						<div class="description">
+							<h2>Chronology</h2>
+							<h3>New office in Seoul, South Korea – 2014</h3>
+							<div class="span span_text_detailt"><p style="text-align: justify;"><span style="font-size: 12pt;">RCI opens its second office abroad in Seoul, South Korea.</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="owl-item" style="width: 1349px; margin-right: 10px;">
+					<div class="chronology-item text-center item">
+						<img width="1366" height="636" src="http://rciapac.com/wp-content/uploads/2017/04/OB82160.jpg" class="attachment-1366x676 size-1366x676 wp-post-image" alt="" srcset="http://rciapac.com/wp-content/uploads/2017/04/OB82160.jpg 1440w, http://rciapac.com/wp-content/uploads/2017/04/OB82160-300x140.jpg 300w, http://rciapac.com/wp-content/uploads/2017/04/OB82160-768x357.jpg 768w, http://rciapac.com/wp-content/uploads/2017/04/OB82160-1024x476.jpg 1024w, http://rciapac.com/wp-content/uploads/2017/04/OB82160-193x90.jpg 193w, http://rciapac.com/wp-content/uploads/2017/04/OB82160-107x50.jpg 107w" sizes="(max-width: 1366px) 100vw, 1366px">
+						<div class="description">
+							<h2>Chronology</h2>
+							<h3>RCI is Demandware’s LINK Service Partner – 2015</h3>
+							<div class="span span_text_detailt"><p style="text-align: justify;"><span style="font-size: 12pt;">In 2015, Demandware (now Salesforce Commerce Cloud) certified RCI as a LINK Service Partner. Thanks to their expertise, RCI can propose to its clients the Salesforce Commerce Cloud E-commerce solution to build online stores.</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="owl-item" style="width: 1349px; margin-right: 10px;">
+					<div class="chronology-item text-center item">
+						<img width="1366" height="636" src="http://rciapac.com/wp-content/uploads/2017/04/OMS-Pro-Chronology.jpg" class="attachment-1366x676 size-1366x676 wp-post-image" alt="" srcset="http://rciapac.com/wp-content/uploads/2017/04/OMS-Pro-Chronology.jpg 1440w, http://rciapac.com/wp-content/uploads/2017/04/OMS-Pro-Chronology-300x140.jpg 300w, http://rciapac.com/wp-content/uploads/2017/04/OMS-Pro-Chronology-768x357.jpg 768w, http://rciapac.com/wp-content/uploads/2017/04/OMS-Pro-Chronology-1024x476.jpg 1024w, http://rciapac.com/wp-content/uploads/2017/04/OMS-Pro-Chronology-193x90.jpg 193w, http://rciapac.com/wp-content/uploads/2017/04/OMS-Pro-Chronology-107x50.jpg 107w" sizes="(max-width: 1366px) 100vw, 1366px">
+						<div class="description">
+							<h2>Chronology</h2>
+							<h3>RCI develops OMS Pro – 2016</h3>
+							<div class="span span_text_detailt"><p style="text-align: justify;"><span style="font-size: 12pt;">In 2016, RCI Global Services developed its own OMS solution called OMS Pro. It is integrated with Salesforce Commerce Cloud E-commerce solution and other back-end solutions as ERP, CRM or POS. With OMS Pro, retailers have a global view on online orders, inventory or client database. It is available in several languages and allows to manage different brands in different countries.</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="owl-nav">
+			<div class="owl-prev disabled"></div>
+			<div class="owl-next"></div>
+		</div>
+	</div>
+</section> -->
