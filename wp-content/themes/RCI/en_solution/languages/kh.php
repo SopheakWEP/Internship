3fd67443e38b6bbde1b336a34213de810eefb1d1
devit-chea <davit.chea123@gmail.com>
2018-07-04 13:13:07 +0700

    add translate fiel

--- wp-content/themes/RCI/en_solution/languages/kh.php
+++ wp-content/themes/RCI/en_solution/languages/kh.php
@@ -7,6 +7,108 @@
 		"welcome" => "សូមស្វារគមន៏ទំព័រការប្ដូរភាសា​",
 		"description" => "ទំព័រនេះនឹងបង្ហានូវការប្ដូរភាសា​ក្នុងទំព័រនេះ",
 		"English" => "អង់គ្លេស",
-		"khmer" => "ខ្មែរ"
+		"khmer" => "ខ្មែរ",
+
+		/* 
+		=
+		=
+		=section 1 RCI Solution
+		=
+		=
+		=*/
+		/* section 1.1 */
+		"solution-1.1" => "Many solutions, one objective",
+		/* section 1.2*/
+		"solution-1.2" => "To offer innovative IT solutions to retailers.",
+		/* section 1.2 */
+		"solution-1.3" => "As technology is in constant evolution, RCI dedicates its work to improve retailers’ productivity in APAC.",
+
+		/* 
+		=
+		=
+		=section 2 RCI Solution
+		=
+		=
+		=*/
+		/* section 2.1 */
+		"solution-2.1" => "Challenges in contemporary retail",
+		/* section 2.2*/
+		"solution-2.2" => "RCI Global Services thoroughly understands retailers’ business concerns and challenges: Optimize customer experience and satisfaction, retailers increasingly focus on perfecting direct-to-consumer operations through different channels. With RCI expertise, retailers overcome these challenges and modernize their business. How can RCI help retailers?",
+		/* section 2.3 */
+		"solution-2.3" => "Set up a new strategy to assure a seamless and consistent consumer experience across both online and offline channels.",
+		/* section 2.4 */
+		"solution-2.4" => "Integrate online and offline channels in key markets or countries and manage different brands in different countries.",
+		/* section 2.5 */
+		"solution-2.5" => "Create IT groundbreaking solutions to deliver omnichannel retailing.",
+
+
+		/* 
+		=
+		=
+		=section 3 RCI Solution
+		=
+		=
+		=*/
+		/* section 3.1 */
+		"solution-3.1" => "O2O expert",
+		/* section 3.2 */
+		"solution-3.2" => "In the framework of an omnichannel strategy, O2O services aim to generate more online and in-store traffic by creating a unified and connected business. The services display all the purchasing process for different channels: web, mobile application or in-store. In the end, the retailers enhance their business and sales by providing a better customer experience.",
+		/* section 3.3 */
+		"solution-3.3" => "Online to Offline",
+		/* section 3.4 */
+		"solution-3.4" => "Online-to-Offline services aim to attract customers from online stores to physical stores. For that, retailers display online information regarding stores inventory items. To increase customer experience, shop assistants have access to shoppers’ online account and wish list to provide personalized items. These products can be purchased or booked online, then picked up or tried at stores. When an item is purchased online, it becomes possible to return it at store. Finally, shop assistants can increase shoppers’ loyalty by providing loyalty card or online promotion with printable vouchers.",
+		/* section 3.5 */
+		"solution-3.5" => "Stock visibility",
+		/* section 3.6 */
+		"solution-3.6" => "Click & collect",
+		/* section 3.7 */
+		"solution-3.7" => "Click & reserve",
+		/* section 3.8 */
+		"solution-3.8" => "Customers & guided selling",
+		/* section 3.9 */
+		"solution-3.9" => "Wish list",
+		/* section 3.10 */
+		"solution-3.10" => "Buy online & return in-store",
+		/* section 3.11 */
+		"solution-3.11" => "Loyalty",
+		/* section 3.12 */
+		"solution-3.12" => "Coupon & voucher",
+		/* section 3.13 */
+		"solution-3.13" => "Offline to Online",
+		/* section 3.14 */
+		"solution-3.14" => "Conversely, Offline-to-Online services attract customers from physical stores to E-commerce website. Stores can provide their customers information about others stores inventory. The services personalize the customer journey by subscribing to the newsletter with POS terminals. On tablets, it becomes possible to show the items and create a wish list. Moreover, the system shares the loyalty program between retail shops and e-shops across different countries. Finally, shop assistants can provide coupons or voucher to be used for online shopping and receive the digital receipt.",
+		/* section 3.15 */
+		"solution-3.15" => "Stock level",
+		/* section 3.16 */
+		"solution-3.16" => "Account & newsletter",
+		/* section 3.17 */
+		"solution-3.17" => "Store extension & endless aisle",
+		/* section 3.18 */
+		"solution-3.18" => "Digital receipt",
+		/* section 3.19 */
+		"solution-3.19" => "Buy-in & return anywhere",
+		/* section 3.20 */
+		"solution-3.20" => "Loyalty",
+		/* section 3.21 */
+		"solution-3.21" => "Coupon & voucher",
+		/* 
+		=
+		=
+		=section 4 RCI Solution
+		=
+		=
+		=*/
+		/* section 4.1 */
+		"solution-4.1" => "E-commerce",
+		/* section 4.2 */
+		"solution-4.2" => "For retailers who wish to enhance their business and sales through online platform, RCI Global Services provides  E-commerce solutions with Salesforce Commerce Cloud or Magento. With an online presence, retailers increase their visibility and reach more customers by offering a large range of products. These items can then be fulfilled by local stores and shipped to their clients. Indeed, integrated with a point of sale (POS) software, customers may also pick up their orders in-store. More than an online platform, the website is connected to local stores and acts as a coordinated whole in the context of an omnichannel retailing strategy.",
+		/* section 4.3 */
+		"solution-4.3" => "Order Management System (OMS)",
+		/* section 4.4 */
+		"solution-4.4" => "An OMS is a software used by retailers, which mainly allows them to have a 360° view on their business. The software shows in real-time the status of the orders, from the payment to the shipment. It is a solution integrated on both side front-end and back-end, that is the main component for a unified commerce through different channels. Last but not least, an OMS solution can display the real time inventory stock and manage the replenishment.",
+		/* section 4.5 */
+		"solution-4.5" => "Point of sale (POS)",
+		/* section 4.6 */
+		"solution-4.6" => "A POS software manages local stores operations as the daily sales, the inventory, the price or even the accounting. It is an essential tool that centralizes all the store information and increases productivity. Most of the stores are equipped with a POS. RCI Global Services is a Retail Pro Authorized Business Partner and also provides iVend POS from CitiXsys."
 	);
 ?>
