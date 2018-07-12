9f7824bf8dfa1ed7c13f4d06119c60645ddad882
devit-chea <davit.chea123@gmail.com>
2018-07-12 15:21:03 +0700

    [D0030] config refs D0030

--- wp-content/themes/RCI/en_contact/config.php
+++ wp-content/themes/RCI/en_contact/config.php
@@ -1,19 +1,19 @@
 
-<?php
+<?php 
 session_start();
 if (!isset($_SESSION['lang'])) 
 {
-	$_SESSION['lang'] = 'en';
+	$_SESSION['lang'] = 'kh';
 }
 elseif (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) 
 {
-	if ($_GET['lang'] == "en") 
-	{
-		$_SESSION['lang'] = "en";
-	}
-	elseif ($_GET['lang'] == "kh")
+	if ($_GET['lang'] == "kh") 
 	{
 		$_SESSION['lang'] = "kh";
+	}
+	elseif ($_GET['lang'] == "en")
+	{
+		$_SESSION['lang'] = "en";
 	}
 }
 require_once "languages/".$_SESSION['lang'].".php";
